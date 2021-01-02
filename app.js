ff45e1c269877ca924a46baaae759ee4439a2aa8
Mark Kenneth G. Esguerra <esguerrakenneth@gmail.com>
2021-01-02 21:21:00 +0800

    Initial Commit

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 })
+
+const fbPageBtn = document.getElementById('fb-page-btn');
+
+fbPageBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  window.open('https://www.facebook.com/groups/459827210733811', '_blank')
+})
+
+const getStartedBtn = document.getElementById('get-started-btn');
+
+getStartedBtn.addEventListener('click', function(){
+  document.getElementById('about').scrollIntoView();
+})
